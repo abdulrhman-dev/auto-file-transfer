c97f1b22dd94a0db004991af91f6de413f319a0c
abdulrhman-dev <abdulrhman.jalal.dev@gmail.com>
2024-07-11 18:24:31 +0300

    Create the file transfer script files

--- client.py
+++ client.py
@@ -6,35 +6,58 @@ import json
 from tqdm import tqdm
 
 
-IP = socket.gethostbyname(socket.gethostname())
+IP = '127.0.0.1'
 PORT = 4456
-ADDR = (IP, PORT)
 SIZE = 1024
 FORMAT = "utf-8"
-FILENAME = "202407-02-temp.xlsx"
-FILESIZE = os.path.getsize(FILENAME)
+SEND_LOCATION = 'E:\\Projects\\auto-file-transfer\send'
+
+
+def list_files_walk(start_path='.'):
+    found_files = []
+    if (not start_path.endswith('\\')):
+        start_path = start_path + '\\'
+
+    for root, dirs, files in os.walk(start_path):
+        for file in files:
+            if (file.startswith('.')):
+                continue
+            directory_path = root.replace(start_path, "")
+            filepath = os.path.join(directory_path, file)
+            found_files.append(
+                {'directory_path': directory_path, 'filepath': filepath})
+
+    return found_files
 
 
 def main():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(ADDR)
+    client.connect((IP, PORT))
 
-    print(f'[CLIENT] Sent file info')
-    client.send(json.dumps(
-        {'filename': FILENAME, 'size': FILESIZE}).encode(FORMAT))
+    send_files = list_files_walk(SEND_LOCATION)
+    for file in send_files:
+        read_path = os.path.join(SEND_LOCATION, file['filepath'])
+        filesize = os.path.getsize(read_path)
 
-    msg = client.recv(SIZE).decode(FORMAT)
-    print(f"SERVER: {msg}")
+        client.send(json.dumps(
+            {'filepath':  file['filepath'], 'directory_path': file['directory_path'], 'filesize': filesize, 'files_len': len(send_files)}).encode(FORMAT))
+        print(f"[SERVER] {client.recv(SIZE).decode(FORMAT)}")
 
-    bar = tqdm(range(FILESIZE), f"Sending {
-               FILENAME}", unit="B", unit_scale=True, unit_divisor=SIZE)
+        bar = tqdm(range(filesize), f"Sending {
+            file['filepath']}", unit="B", unit_scale=True, unit_divisor=SIZE)
+        with open(read_path, "rb") as file:
+            while True:
+                chunk = file.read(SIZE)
 
-    with open(FILENAME, "rb") as file:
-        while chunk := file.read(SIZE):
-            client.send(chunk)
-            bar.update(len(chunk))
+                if not chunk:
+                    break
 
-    bar.close()
+                client.sendall(chunk)
+                bar.update(len(chunk))
+
+        print(f"[SERVER] {client.recv(SIZE).decode(FORMAT)}")
+        bar.close()
+
     client.close()
 
 
--- server.py
+++ server.py
@@ -1,45 +1,88 @@
 import socket
-from tqdm import tqdm
+# from tqdm import tqdm
+import os
 import json
 
-IP = socket.gethostbyname(socket.gethostname())
-PORT = 4456
-ADDR = (IP, PORT)
+app_path = os.path.join(os.getenv('LOCALAPPDATA'), 'auto-transfer')
+
+if not os.path.exists(app_path):
+    os.makedirs(app_path)
+    with open(os.path.join(app_path, 'settings.json'), 'w') as file:
+        json.dump({
+            "ip": "127.0.0.1",
+            "port": "4456",
+            "export_location": os.path.join(app_path, 'files')
+        }, file)
+    # print("[+] Created App Directory successfully!")
+
+
+with open(os.path.join(app_path, 'settings.json'), 'r') as file:
+    settings = json.load(file)
+
+
+IP = settings['ip']
+PORT = int(settings['port'])
+EXPORT_LOCATION = settings['export_location']
 SIZE = 1024
-FORMAT = "utf-8"
+FORMAT = 'utf-8'
 
 
 def main():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(ADDR)
+    server.bind((IP, PORT))
     server.listen()
-    print("[+] Listening...")
+    # print("[+] Listening...")
+
     while True:
-        conn, addr = server.accept()
-        print(f"[+] Client connected from {addr[0]}:{addr[1]}")
+        try:
+            conn, addr = server.accept()
+            # print(f"[+] Client connected from {addr[0]}:{addr[1]}")
 
-        data = json.loads(conn.recv(SIZE).decode(FORMAT))
-        FILENAME = data['filename']
-        FILESIZE = data['size']
+            downloaded_files = None
 
-        print("[+] Filename and filesize received from the client.")
-        conn.send("Filename and filesize received".encode(FORMAT))
+            while downloaded_files is None or downloaded_files > 0:
+                str_value = conn.recv(SIZE).decode(FORMAT)
 
-        bar = tqdm(range(FILESIZE), f"Receiving {
-            FILENAME}", unit="B", unit_scale=True, unit_divisor=SIZE)
+                data = json.loads(str_value)
 
-        with open(f"recv_{FILENAME}", "wb") as f:
-            while True:
-                data = conn.recv(SIZE)
+                if (downloaded_files is None):
+                    downloaded_files = data['files_len']
 
-                if not data:
-                    break
+                filepath = data['filepath']
+                directory_path = data['directory_path']
+                filesize = data['filesize']
 
-                f.write(data)
-                bar.update(len(data))
+                # print(f"[+] {filepath} received from the client.")
+                conn.send(f"received {filepath}".encode(FORMAT))
 
-        bar.close()
-        conn.close()
+                # bar = tqdm(range(filesize), f"Receiving {
+                #     filepath}", unit="B", unit_scale=True, unit_divisor=SIZE)
+
+                if not os.path.exists(os.path.join(EXPORT_LOCATION, directory_path)):
+                    os.makedirs(os.path.join(EXPORT_LOCATION, directory_path))
+
+                accumlated_size = 0
+                with open(os.path.join(EXPORT_LOCATION, filepath), "wb") as f:
+                    while True:
+                        chunk = conn.recv(SIZE)
+                        accumlated_size += len(chunk)
+
+                        if not chunk:
+                            break
+
+                        f.write(chunk)
+
+                        # bar.update(len(chunk))
+                        if accumlated_size >= filesize:
+                            break
+                conn.send(f"downloaded {filepath} successfully".encode(FORMAT))
+                # bar.close()
+                downloaded_files -= 1
+
+            conn.close()
+        except:
+            conn.close()
+            # print('[-] Disconnected the client')
 
 
 if __name__ == "__main__":
